8d57883649d44cf739acf2d64415967f0a226ecf
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-08-15 00:28:57 +0600

    login page javascript added and dashboard design completed

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const loginInfo = {
+  email: "ashikujjamanhimel@gmail.com",
+  password: "Password&1234",
+};
+
+const emailInput = document.getElementById("login-email");
+const passwordInput = document.getElementById("login-password");
+const formMassage = document.getElementById('form-massage');
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  if (emailInput.value.toLowerCase() === loginInfo.email && passwordInput.value === loginInfo.password) {
+    location.href = "dashboard.html";
+  }
+  else {
+    passwordInput.value = "";
+    formMassage.style.display = "block";
+    setTimeout(function() {
+      formMassage.style.display = "none";
+    }, 3000);
+  }
+  event.preventDefault();
+});
+
+console.log(location.href);
