610f3be50c702b53168864a37b595ba9a2fae5de
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-08-14 22:03:43 +0600

    tailwind project setup done with daisy ui

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['night']
+  }
+}
+
